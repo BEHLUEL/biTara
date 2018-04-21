6c378fc56bfc06a192a2ae301f0ae7406863d12e
Behlül Sarıkaya <34069684+BEHLUEL@users.noreply.github.com>
2018-04-21 19:47:47 +0300

    GirisSayfasi Eklendi

---
+++ biTara/components/GirisSayfasi.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Alert, AppRegistry, Button, StyleSheet, View, Text } from 'react-native';
+
+class GirisEkrani extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!');
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <Text style={styles.welcome}>biTara
+        </Text>
+        <View style={styles.buttonContainer}>
+          <Button
+           
+            title="Mobil"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="İnternet"
+            color="#841584"
+          />
+        </View>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 10
+  },
+  welcome: {
+    fontSize: 60,
+    textAlign: 'center',
+    margin: 30,
+    marginBottom: 70,
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+});
+
+// skip this line if using Create React Native 
+export default GirisEkrani;
