3ddc384b5f2c03ad90f0e3509c4511867c0b4e41
Behlül Sarıkaya <34069684+BEHLUEL@users.noreply.github.com>
2018-04-23 18:39:52 +0300

    MobilAyrıntı sayfası oluşturuldu

---
+++ biTara/src/components/MobilAyrinti.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+
+class MobilAyrinti extends Component {
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default MobilAyrinti;
