e3159f969dba77e4f987af3e51b377f2dfaf5cc4
Behlül Sarıkaya <34069684+BEHLUEL@users.noreply.github.com>
2018-04-27 18:30:30 +0300

    router Arka plan çalışması eklendi

--- biTara/Router.js
+++ biTara/Router.js
@@ -8,19 +8,56 @@ import InternetAyrinti from './src/components/InternetAyrinti';
 import MobilKisitSayfasi from './src/components/MobilKisitSayfasi';
 import InternetKisitSayfasi from './src/components/InternetKisitSayfasi';
 
-const RouterComponent = () => {
-    return(
-        <Router>
-            <Scene key="bitara">
-                <Scene key="mobiltarifeler" component={MobilTarifeler} title="Mobil Tarifeler" />
-                <Scene key="internettarifeler" component={InternetTarifeler} title="Internet Tarifeler" />
-                <Scene key="mobilayrinti" component={MobilAyrinti} title="Mobil Tarife Ayrıntı" />
-                <Scene key="internetayrinti" component={InternetAyrinti} title="Internet Tarife Ayrıntı" />
-                <Scene key="mobilkisitsayfasi" component={MobilKisitSayfasi} title="Mobil Tarife Arama" />
-                <Scene key="internetkisitsayfasi" component={InternetKisitSayfasi} title="Internet Tarife Arama" />
+const RouterComponent = () => (
+        <Router style={styles.container} >
+            <Scene style={styles.container} key="bitara">
+                <Scene 
+                
+                key="mobiltarifeler" 
+                component={MobilTarifeler} 
+                title="Mobil Tarifeler" 
+                />
+
+                <Scene 
+                key="internettarifeler" 
+                component={InternetTarifeler}
+                title="Internet Tarifeler" 
+                />
+                
+                <Scene 
+                key="mobilayrinti" 
+                component={MobilAyrinti} 
+                title="Mobil Tarife Ayrıntı" 
+                />
+                <Scene 
+                key="internetayrinti" 
+                component={InternetAyrinti} 
+                title="Internet Tarife Ayrıntı" 
+                />
+                
+                <Scene 
+                key="mobilkisitsayfasi" 
+                component={MobilKisitSayfasi} 
+                title="Mobil Tarife Arama" 
+                />
+
+                <Scene 
+                key="internetkisitsayfasi" 
+                component={InternetKisitSayfasi} 
+                title="Internet Tarife Arama" 
+                />
+
             </Scene>
         </Router>
     );
-};
+const styles = {
+    container: {
+      flex: 1,
+      //justifyContent: 'center',
+      //alignItems: 'center',
+      backgroundColor: '#002233',
+    },
+    
+  };
 
 export default RouterComponent;
