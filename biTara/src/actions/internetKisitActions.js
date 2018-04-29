c9a8960483206985e9f5bf2cdb349176ecde749f
Behlül Sarıkaya <34069684+BEHLUEL@users.noreply.github.com>
2018-04-29 16:21:23 +0300

    internetKısıt actions'a başlandı

---
+++ biTara/src/actions/internetKisitActions.js
@@ -0,0 +1,11 @@
+import { Actions } from 'react-native-router-flux';
+import { INTERNET_KISIT_CHANGED } from './types';
+
+export const internetKisitChange = ({ props, value }) => {
+    return (dispatch) => {
+        dispatch({
+            type: INTERNET_KISIT_CHANGED,
+            payload: { props, value }
+        });
+    };
+};
---
+++ biTara/src/actions/types.js
@@ -0,0 +1,1 @@
+export const INTERNET_KISIT_CHANGED = 'internet_kisit_changed';
