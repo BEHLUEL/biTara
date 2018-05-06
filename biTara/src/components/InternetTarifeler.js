8ba65e09f935221e5852f0405a636c05a3f5b20b
yozdel <ozdelyasin@gmail.com>
2018-05-06 14:59:29 +0300

    İnternet tarifeler state alındı.

--- biTara/src/components/InternetTarifeler.js
+++ biTara/src/components/InternetTarifeler.js
@@ -3,6 +3,11 @@ import React, { Component } from 'react';
 import { Text, Button, ScrollView, View } from 'react-native';
 //import Card from '../ortak/Card';
 //import CardSection from '../ortak/CardSection';
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+import { connect } from 'react-redux';
+import RadioButton from 'react-native-radio-button';
+//import internetAra from '../actions/internetKisitActions';
 
 
 class InternetTarifeler extends Component {
@@ -72,4 +77,36 @@ const styles = {
       }
 };
 
-export default InternetTarifeler;
+const mapToStateProps = ({ internetResponse }) => {
+  const { tumu,
+      turktelekom,
+      superonline,
+      kablonet,
+      turknet,
+      saglayicilar,
+      limit,
+      hiz,
+      gb,
+      tv,
+      tel,
+      yalin,
+      taahhut } = internetResponse;
+
+  return {
+      tumu,
+      turktelekom,
+      superonline,
+      kablonet,
+      turknet,
+      saglayicilar,
+      limit,
+      hiz,
+      gb,
+      tv,
+      tel,
+      yalin,
+      taahhut
+  };
+};
+//export default InternetKisitSayfasi;
+export default connect(mapToStateProps, { })(InternetTarifeler);
