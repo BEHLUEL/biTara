bcd71d2688ef2df9635a6a3e513bc4be1e9d35c6
Behlül Sarıkaya <34069684+BEHLUEL@users.noreply.github.com>
2018-05-09 00:48:06 +0300

    Update types.js

--- biTara/src/actions/types.js
+++ biTara/src/actions/types.js
@@ -2,3 +2,5 @@ export const INTERNET_KISIT_CHANGED = 'internet_kisit_changed';
 export const MOBIL_KISIT_CHANGED = 'mobil_kisit_changed';
 
 export const MOBIL_LIST_DATA_SUCCESS = 'mobil_list_data_success';
+
+export const INTERNET_LIST_DATA_SUCCESS = 'internet_list_data_success';
