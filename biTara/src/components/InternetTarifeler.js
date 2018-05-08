f6d5eca716f861e3c72ce44c4d4232c5fdbc4845
Behlül Sarıkaya <34069684+BEHLUEL@users.noreply.github.com>
2018-05-09 01:05:42 +0300

    Update InternetTarifeler.js

--- biTara/src/components/InternetTarifeler.js
+++ biTara/src/components/InternetTarifeler.js
@@ -1,6 +1,6 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-
-import { Text, Button, ScrollView, View } from 'react-native';
+import { Text, Button, ScrollView, View, ListView } from 'react-native';
 //import Card from '../ortak/Card';
 //import CardSection from '../ortak/CardSection';
 import firebase from 'firebase';
@@ -8,55 +8,50 @@ import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 import RadioButton from 'react-native-radio-button';
 //import internetAra from '../actions/internetKisitActions';
-
+import { internetListData } from '../actions';
+import ListItem from './internetListItem';
 
 class InternetTarifeler extends Component {
+    componentWillMount() {
+        this.props.internetListData();
+        this.createDataSource(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.createDataSource(nextProps);
+    }
+    createDataSource({ internetArray }) {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+        this.dataSource = ds.cloneWithRows(internetArray);
+    }
     clickLogin() {
-        Actions.internetayrinti();
+
+    }
+    renderRow(tarife) {
+        return <ListItem tarife={tarife} />;
     }
     render() {
         return (
-         <ScrollView style={{ backgroundColor: '#F4F6F9' }}>
-           
-          </ScrollView>        
+            <ScrollView style={{ backgroundColor: '#F4F6F9' }}>
+                <ListView
+                    enableEmptySections
+                    dataSource={this.dataSource}
+                    renderRow={this.renderRow}
+                />
+            </ScrollView>
         );
     };
 };
 
-const styles = {
-    
+const mapToStateProps = ({ internetdataResponse }) => {
+    const internetArray = _.map(internetdataResponse, (val, uid) => {
+        return { ...val, uid };
+    });
+    return { internetArray };
 };
 
-const mapToStateProps = ({ internetResponse }) => {
-  const { tumu,
-      turktelekom,
-      superonline,
-      kablonet,
-      turknet,
-      saglayicilar,
-      limit,
-      hiz,
-      gb,
-      tv,
-      tel,
-      yalin,
-      taahhut } = internetResponse;
-
-  return {
-      tumu,
-      turktelekom,
-      superonline,
-      kablonet,
-      turknet,
-      saglayicilar,
-      limit,
-      hiz,
-      gb,
-      tv,
-      tel,
-      yalin,
-      taahhut
-  };
-};
 //export default InternetKisitSayfasi;
-export default connect(mapToStateProps, { })(InternetTarifeler);
+export default connect(mapToStateProps, { internetListData })(InternetTarifeler);
+         
