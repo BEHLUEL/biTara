a202a84bde39f0ecd2950d45c558e70a6d7efe6f
Behlül Sarıkaya <34069684+BEHLUEL@users.noreply.github.com>
2018-05-09 00:48:48 +0300

    Add files via upload

---
+++ biTara/src/reducers/internetDataReducers.js
@@ -0,0 +1,16 @@
+import { INTERNET_LIST_DATA_SUCCESS } from '../actions/types';
+
+const INITIAL_STATE = {
+    
+};
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case INTERNET_LIST_DATA_SUCCESS:
+        //console.log(action);
+            return action.payload;
+        
+        default:
+            return state;
+    }
+};
