55f8c8e7f78f9171f09b76da5f7c1b52b8d02f2a
Behlül Sarıkaya <34069684+BEHLUEL@users.noreply.github.com>
2018-05-09 00:47:21 +0300

    Update internetKisitActions.js

--- biTara/src/actions/internetKisitActions.js
+++ biTara/src/actions/internetKisitActions.js
@@ -1,5 +1,6 @@
 import { Actions } from 'react-native-router-flux';
-import { INTERNET_KISIT_CHANGED } from './types';
+import firebase from 'firebase';
+import { INTERNET_KISIT_CHANGED, INTERNET_LIST_DATA_SUCCESS } from './types';
 
 export const internetKisitChange = ({ props, value }) => {
     return (dispatch) => {
@@ -10,7 +11,11 @@ export const internetKisitChange = ({ props, value }) => {
     };
 };
 
-export const internetAra = ({ props, value }) => {
-    //firebase
+export const internetListData = () => {
+    return (dispatch) => {
+        firebase.database().ref('/Tarifeler/Internet/')
+        .on('value', snapshot => {
+            dispatch({ type: INTERNET_LIST_DATA_SUCCESS, payload: snapshot.val() });
+        });    
+    };
 };
-
